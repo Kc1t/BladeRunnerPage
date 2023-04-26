2496500e6e9d62d6ca43546e8d2e3f43366a383b
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-04-26 09:18:32 -0300

    tgl btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,18 @@ const start = document.getElementById("start");
 const audio = new Audio("/assets/song.mp3");
 audio.volume = 0.6;
 let isPlaying = true;
+let img = document.getElementById('play')
 
 start.addEventListener("click", function () {
   if (isPlaying) {
     audio.pause();
+    img.setAttribute('src', 'assets/img/starts.svg');
   } else {
     audio.play();
+     img.setAttribute('src', 'assets/img/pause.svg')
   }
   isPlaying = !isPlaying;
 });
+
+start.addEventListener("click", toggleAudio);
+img.addEventListener("click", toggleAudio);
